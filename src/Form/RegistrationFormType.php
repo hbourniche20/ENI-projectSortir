d69fa34a14fd8b48c8b62f33ab2ec9fff45aeee4
rdelauna2020 <ryan.delaunay2020@campus-eni.fr>
2021-10-20 11:20:46 +0200

    Ajout ville dans inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,12 +3,12 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -39,6 +39,7 @@ class RegistrationFormType extends AbstractType
             ->add('prenom')
             ->add('nom')
             ->add('tel')
+            ->add('ville', EntityType::class, ['class' => Ville::class, 'label' => 'Ville de ratachement'])
         ;
     }
 
