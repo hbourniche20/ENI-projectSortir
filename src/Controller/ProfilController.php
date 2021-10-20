c4a488d7bbc4d660491b05c283226f3e677ed201
Valentin <vhenique@gmail.com>
2021-10-20 16:38:29 +0200

    Mise en place de la gestion de la photo de profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,6 +2,7 @@
 
     namespace App\Controller;
 
+    use App\Entity\Image;
     use App\Entity\User;
     use App\Form\UserFormType;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -40,6 +41,8 @@
                 $email = trim(strip_tags($form->get('email')->getData()));
                 $confirmation = trim(strip_tags($form->get('confirmation')->getData()));
                 $password = trim(strip_tags($form->get('password')->getData()));
+                $photo = $form->get('photo')->getData();
+                $photoExtension = $photo->guessExtension();
 
                 //vérifie les données
                 // email
@@ -54,8 +57,36 @@
                 if (strlen($password) < 6) {
                     array_push($errors, 'Le mot de passe doit faire plus de 6 caractères.');
                 }
+                // Vérification de l'extension de la photo
+                if($photoExtension !== 'jpg' && $photoExtension !== 'jpeg' && $photoExtension !== 'png' && $photoExtension !== 'gif'){
+                    array_push($errors, 'Le format de l\'image est invalide. Utiliser .jpg, .jpeg, .png ou .gif');
+                }
+
+                // Vérification de la taille de la photo
+                list($width, $height) = getimagesize($photo);
+                if($width > 1920 || $height > 1080){
+                    array_push($errors, 'La dimension de l\'image ne doit pas être supérieur à 1920x1080');
+                }
 
                 if (empty($errors)) {
+                    $entityManager = $this->getDoctrine()->getManager();
+                    // verification de si une photo existe
+
+                    // on gère la photo :
+
+                    $fichier = $user->getPseudo() . '.' . $photoExtension;
+                    //copie de l'image
+                    $photo->move(
+                        $this->getParameter('images_directory'),
+                        $fichier
+                    );
+
+                    // On stock l'image en bdd si elle n'existe pas
+                    if(is_null($user->getImage())){
+                        $img = new Image();
+                        $img->setName($fichier);
+                        $user->setImage($img);
+                    }
 
                     // on hash et on upload
                     $user->setPassword(
@@ -64,9 +95,10 @@
                             $form->get('password')->getData()
                         )
                     );
-                    $entityManager = $this->getDoctrine()->getManager();
                     $entityManager->persist($user);
                     $entityManager->flush();
+                    // Attente de 1 seconde pour laisser le temps à l'img d'etre remplacer
+                    sleep(1);
                     return $this->redirectToRoute('profil');
                 }
                 $user = $this->getUserBySession();
