2b7e6ef426e51d50322921453887f445c864c416
rdelauna2020 <ryan.delaunay2020@campus-eni.fr>
2021-10-25 09:11:50 +0200

    MAJ donnee

--- public/scriptSQL/initBdd.sql
+++ public/scriptSQL/initBdd.sql
@@ -24,20 +24,48 @@ insert into site(ville_id, nom, rue) values(6, 'McDonalds', 'Rn Avenue Du Génra
 
 -- Init User password pour tous les utilisateurs est "password"
 INSERT INTO user(id, ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (0, 1, 'email@inconnu.inc', 'Inconnu', 'Inconnu','Inconnu','0000000000','[]','');
-INSERT INTO user(ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (1, 'pseu@gmail.com', 'Pseu', 'Valentin','Moi','0698653278','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
-INSERT INTO user(ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (1, 'dop@gmail.com', 'Dop', 'Hugo','Toi','0678451298','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
-INSERT INTO user(ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (2, 'ryu@free.fr', 'Ryu', 'Ryan','Lui','0696857432','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
-INSERT INTO user(ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (2, 'mario@gmail.com', 'Mario', 'Antoine','Eux','0636251498','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
-INSERT INTO user(ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (3, 'peach@gmail.com', 'Peach', 'Océane','Elle','0651545758','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
-INSERT INTO user(ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (4, 'doomguy@gmx.fr', 'DoomGuy', 'Audrey','Autre','0653565952','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
-INSERT INTO user(ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (5, 'rincevent@gmail.com', 'RinceVent', 'Aurore','Nom','0645953512','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
-INSERT INTO user(ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (6, 'sonic@gmail.com', 'Sonic', 'Claire','Jesaispas','0687324516','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
+INSERT INTO user(id,ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (1,1, 'pseu@gmail.com', 'Pseu', 'Valentin','Moi','0698653278','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
+INSERT INTO user(id,ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (2,1, 'dop@gmail.com', 'Dop', 'Hugo','Toi','0678451298','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
+INSERT INTO user(id,ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (3,2, 'ryu@free.fr', 'Ryu', 'Ryan','Lui','0696857432','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
+INSERT INTO user(id,ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (4,2, 'mario@gmail.com', 'Mario', 'Antoine','Eux','0636251498','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
+INSERT INTO user(id,ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (5,3, 'peach@gmail.com', 'Peach', 'Océane','Elle','0651545758','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
+INSERT INTO user(id,ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (6,4, 'doomguy@gmx.fr', 'DoomGuy', 'Audrey','Autre','0653565952','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
+INSERT INTO user(id,ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (7,5, 'rincevent@gmail.com', 'RinceVent', 'Aurore','Nom','0645953512','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
+INSERT INTO user(id,ville_id, email, pseudo, prenom, nom, tel, roles, password) VALUES (8,6, 'sonic@gmail.com', 'Sonic', 'Claire','Jesaispas','0687324516','[]','$2y$13$fPxr.WdTTNoVzV9nIPlOquD/tXNcdbJsfVsgKIWYCKXjIkFR.6bm6');
 
 -- Init Sorties
-insert into sortie(ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description) values (1, 1, 1, 2, 'Dejeuner à la crêperie', current_timestamp, current_date, 6, 120, 'Déjeuné à la Crêperie du Vieux Qumpier. C''est Didier qui régale' );
-insert into sortie(ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description) values (3, 1, 5, 1, 'Café du matin', current_timestamp, current_date, 12, 60, 'Café au parc, détente et baignade');
-insert into sortie(ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description) values (2, 3, 4, 1, 'Détour par la Taverne', current_timestamp, current_date, 8, 80, 'Petite bière à la taverne de la gare');
-insert into sortie(ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description) values (1, 2, 2, 1, 'Formation de défense', current_timestamp, current_date, 24, 360, 'Entrainement de self defense auprès de la police de Rennes');
-insert into sortie(ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description) values (5, 6, 7, 12, 'Anniversaire de Bertrand', current_timestamp, current_date, 12, 150, 'Anniversaire de Bertrand au MacDonald de Vitry-le-François, seul MacDonald qui a accepté recevoir Bertrand pour ses 52 ans');
-insert into sortie(ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description) values (5, 1, 7, 3, 'Un peu de culture', current_timestamp, current_date, 8, 60, 'Petit tour à la FNAC rayon livre, ON EST PAS LA POUR ACHETER DES JEUX-VIDEOS');
-insert into sortie(ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description) values (4, 1, 6, 3, 'Pause video-games', current_timestamp, current_date, 15, 90, 'Petite pause aux jeux-vidéals');
+insert into sortie(id,ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description,publiee) values (1,1, 1, 1, 2, 'Dejeuner à la crêperie', current_timestamp, current_date, 6, 120, 'Déjeuné à la Crêperie du Vieux Qumpier. C''est Didier qui régale' ,0);
+insert into sortie(id,ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description,publiee) values (2,3, 1, 5, 1, 'Café du matin', current_timestamp, current_date, 12, 60, 'Café au parc, détente et baignade',1);
+insert into sortie(id,ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description,publiee) values (3,2, 3, 4, 1, 'Détour par la Taverne', current_timestamp, current_date, 8, 80, 'Petite bière à la taverne de la gare',1);
+insert into sortie(id,ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description,publiee) values (4,1, 2, 2, 1, 'Formation de défense', current_timestamp, current_date, 24, 360, 'Entrainement de self defense auprès de la police de Rennes',0);
+insert into sortie(id,ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description,publiee) values (5,5, 6, 7, 12, 'Anniversaire de Bertrand', current_timestamp, current_date, 12, 150, 'Anniversaire de Bertrand au MacDonald de Vitry-le-François, seul MacDonald qui a accepté recevoir Bertrand pour ses 52 ans',0);
+insert into sortie(id,ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description,publiee) values (6,5, 1, 7, 3, 'Un peu de culture', current_timestamp, current_date, 8, 60, 'Petit tour à la FNAC rayon livre, ON EST PAS LA POUR ACHETER DES JEUX-VIDEOS',1);
+insert into sortie(id,ville_organisatrice_id, ville_accueil_id, organisateur_id, site_id, nom, date_sortie, date_limite_inscription, nb_places, duree, description,publiee) values (7,4, 1, 6, 3, 'Pause video-games', current_timestamp, current_date, 15, 90, 'Petite pause aux jeux-vidéals',0);
+
+-- Init Sortie_user
+insert into sortie_user(user_id,sortie_id) values (1,1);
+insert into sortie_user(user_id,sortie_id) values (2,1);
+insert into sortie_user(user_id,sortie_id) values (3,1);
+insert into sortie_user(user_id,sortie_id) values (1,2);
+insert into sortie_user(user_id,sortie_id) values (1,3);
+insert into sortie_user(user_id,sortie_id) values (1,4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
